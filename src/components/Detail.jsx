011dabf31f5398acecbecc9f85af4bdff661ac99
ERehbani <elianleesin@gmail.com>
2023-04-06 12:55:13 -0300

    commit

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 
-const Detail = ({Rick}) => {
+const Detail = () => {
     const {id} = useParams()
-    const [character, setCharacter] = useState()
+    const [character, setCharacter] = useState({})
 
 
 
     useEffect(() => {
+        // axios(`${URL-BASE}/${id}?key=${API_KEY}`).then
         axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
            if (data.name) {
               setCharacter(data);
@@ -22,12 +23,12 @@ const Detail = ({Rick}) => {
 
     return (
         <div>
-            <h2>Name |{Rick.name}</h2>
-            <h2>Status | {character.status}</h2>
-            <h2>Species |{character.species}</h2>
-            <h2>Gender |{character.gender}</h2>
+            <h2>Name |{character?.name}</h2>
+            <h2>Status | {character?.status}</h2>
+            <h2>Species |{character?.species}</h2>
+            <h2>Gender |{character?.gender}</h2>
             <h2>Origin| {character.origin?.name}</h2>
-            <h2><img src={character.image} alt="" /></h2>
+            <h2><img src={character?.image} alt="" /></h2>
         </div>
     )
 }
